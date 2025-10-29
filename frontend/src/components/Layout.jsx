9d704da2fbdf6a2aba9d28d80ac067f9a6cd98e2
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-10-29 21:17:26 +0530

    complete src

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,21 +1,19 @@
-import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
-
-const Layout = ({children,showSidebar=false}) => {
+const Layout = ({ children, showSidebar = false }) => {
   return (
-    <div className='min-h-screen'>
-        <div className='flex'>
-            {showSidebar && <Sidebar/>}
-            <div className="flex-1 flex flex-col">
-                <Navbar/>
-                <main className="flex-1 overflow-y-auto">
-                    {children}
-                </main>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div className="min-h-screen">
+      <div className="flex">
+        {showSidebar && <Sidebar />}
 
-export default Layout
+        <div className="flex-1 flex flex-col">
+          <Navbar />
+
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Layout;
--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,10 +1,12 @@
-import {LoaderIcon} from "lucide-react"
-const PageLoader = () => {
-  return (
-    <div className='min-h-screen flex items-center justify-center'>
-        <LoaderIcon className="animate-spin size-10 text-primary"/>
-    </div>
-  )
-}
+import { LoaderIcon } from "lucide-react";
+import { useThemeStore } from "../store/useThemeStore";
 
-export default PageLoader
+const PageLoader = () => {
+  const { theme } = useThemeStore();
+  return (
+    <div className="min-h-screen flex items-center justify-center" data-theme={theme}>
+      <LoaderIcon className="animate-spin size-10 text-primary" />
+    </div>
+  );
+};
+export default PageLoader;
